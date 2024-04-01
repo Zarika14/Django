3fcb3c6731717eb18813911699d05569ecfcd7ef
Zarika14 <91460861+Zarika14@users.noreply.github.com>
2024-04-01 18:12:46 +0530

    .

---
+++ myproject/myapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    # MYInfo = {
+    #     'Name': 'Prashant',
+    #     'Age': 23,
+    #     'State' : 'Himacha Pradesh'
+    # }
+    return render(request,'index.html')
+
+def counter(request):
+    text = request.GET['text']
+    totalWords = len(text.split())
+    return render(request,"counter.html",{'number': totalWords })
